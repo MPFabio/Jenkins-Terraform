e15202bb7e8130e69fb5669a06cf9d22f20c5f27
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-08 14:10:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,15 +44,148 @@ provider "azurerm" {
    features {}
 }
 
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+resource "azurerm_resource_group" "webserver" {
+   name = "nginx-server-fabio-${var.environment}"
+   location = var.location
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
+
+resource "azurerm_network_interface_security_group_association" "nsgnic" {
+  network_interface_id      = azurerm_network_interface.webserver.id
+  network_security_group_id = azurerm_network_security_group.allowedports.id
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+resource "azurerm_network_security_group" "allowedports" {
+   name = "allowedports"
+   resource_group_name = azurerm_resource_group.webserver.name
+   location = azurerm_resource_group.webserver.location
+  
+   security_rule {
+       name = "http"
+       priority = 100
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "80"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "https"
+       priority = 200
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "443"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+    security_rule {
+       name = "custom"
+       priority = 400
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "8080"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+   
+   security_rule {
+       name = "ssh"
+       priority = 300
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "22"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+}
+
+resource "azurerm_public_ip" "webserver_public_ip" {
+   name = "webserver_public_ip"
+   location = var.location
+   resource_group_name = azurerm_resource_group.webserver.name
+   allocation_method = "Dynamic"
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
+resource "azurerm_network_interface" "webserver" {
+   name = "nginx-interface"
+   location = azurerm_resource_group.webserver.location
+   resource_group_name = azurerm_resource_group.webserver.name
+
+   ip_configuration {
+       name = "internal"
+       private_ip_address_allocation = "Dynamic"
+       subnet_id = azurerm_subnet.webserver-subnet.id
+       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
+resource "azurerm_linux_virtual_machine" "nginx" {
+   size = var.instance_size
+   name = "nginx-webserver"
+   resource_group_name = azurerm_resource_group.webserver.name
+   location = azurerm_resource_group.webserver.location
+   network_interface_ids = [
+       azurerm_network_interface.webserver.id,
+   ]
+
+   source_image_reference {
+       publisher = "Canonical"
+       offer = "UbuntuServer"
+       sku = "18.04-LTS"
+       version = "latest"
+   }
+
+   computer_name = "nginx"
+   admin_username = "fabio"
+   admin_password = "Azerty-123"
+   disable_password_authentication = false
+
+   os_disk {
+       name = "nginxdisk01"
+       caching = "ReadWrite"
+       #create_option = "FromImage"
+       storage_account_type = "Standard_LRS"
+   }
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
+resource "azurerm_virtual_network" "webserver-net" {
+  name                = "webserver-net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.webserver.location
+  resource_group_name = azurerm_resource_group.webserver.name
+}
+
+resource "azurerm_subnet" "webserver-subnet" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.webserver.name
+  virtual_network_name = azurerm_virtual_network.webserver-net.name
+  address_prefixes       = ["10.0.1.0/24"]
+
+  private_link_service_network_policies_enabled = false
 }
