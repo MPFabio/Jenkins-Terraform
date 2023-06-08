66d9be9d4261168ac7e8aa7e810a33f9c24dbce3
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-08 09:54:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "webserver" {
-   name = "nginx-server-fabio"
+   name = "nginx-server-fabio-${var.environment}"
    location = var.location
 }
 
@@ -85,6 +85,11 @@ resource "azurerm_public_ip" "webserver_public_ip" {
    resource_group_name = azurerm_resource_group.webserver.name
    allocation_method = "Dynamic"
 
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
    depends_on = [azurerm_resource_group.webserver]
 }
 
@@ -108,6 +113,7 @@ resource "azurerm_linux_virtual_machine" "nginx" {
    name = "nginx-webserver"
    resource_group_name = azurerm_resource_group.webserver.name
    location = azurerm_resource_group.webserver.location
+   custom_data = base64encode(file("../azure-webserver/init-script.sh"))
    network_interface_ids = [
        azurerm_network_interface.webserver.id,
    ]
@@ -119,7 +125,7 @@ resource "azurerm_linux_virtual_machine" "nginx" {
        version = "latest"
    }
 
-   computer_name = "vm-fabio"
+   computer_name = "nginx"
    admin_username = "fabio"
    admin_password = "Azerty-123"
    disable_password_authentication = false
@@ -131,6 +137,11 @@ resource "azurerm_linux_virtual_machine" "nginx" {
        storage_account_type = "Standard_LRS"
    }
 
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
    depends_on = [azurerm_resource_group.webserver]
 }
 
@@ -139,6 +150,15 @@ variable "subscription_id" {
    default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
 }
 
+#variable "client_id" {
+#   description = "Azure Client ID"
+#   default = "384ba4e4-fc1a-47f4-bf42-acec6fad1e82"
+#}
+
+#variable "client_secret" {
+#   description = "Azure Client Secret"
+#   default = "lxk8Q~LfBuObGY4frVfc6B_e4Nqf6UMahS-BdaD-"
+#}
 
 variable "tenant_id" {
    description = "Azure Tenant ID"
@@ -157,8 +177,13 @@ variable "location" {
    default = "West Europe"
 }
 
-  
-  resource "azurerm_virtual_network" "webserver-net" {
+variable "environment" {
+   type = string
+   description = "Environment"
+   default = "staging"
+}
+
+resource "azurerm_virtual_network" "webserver-net" {
   name                = "webserver-net"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.webserver.location
