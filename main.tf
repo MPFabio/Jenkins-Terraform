b910203fb8ecb11d7f7ee648be3005b152afe188
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-08 12:50:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "resource_group_name_prefix" {
 
 variable "subscription_id" {
    description = "Azure subscription"
-   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c "
 }
 
 variable "client_id" {
@@ -25,7 +25,7 @@ variable "client_secret" {
 
 variable "tenant_id" {
    description = "Azure Tenant ID"
-   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
 } 
 
 terraform {
