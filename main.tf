b9a76d59395664602a0fbce24ab43197d7afa770
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-08 10:12:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
 terraform {
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
+   required_version = ">= 0.12"
+   required_providers {
+      azurerm = ">3.0"
+   }
 }
 
 provider "azurerm" {
-  features {}
+   subscription_id = var.subscription_id
+   #client_id = var.client_id
+   #client_secret = var.client_secret
+   tenant_id = var.tenant_id
+   skip_provider_registration = true
+   features {}
 }
 
 resource "random_pet" "rg_name" {
