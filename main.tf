85c798ea23a15f67fc869ac49c2486444dfd847a
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-09 09:37:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 provider "azurerm" {
    subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
    client_id = "2ac719ef-3f8b-45ad-b133-8d66e0919476"
-   client_secret = "Uou8Q~3upcTWJlpfcLw1Y39QOnoG_Qq8iTXfLcY0"
+   client_secret = "9rf8Q~iPU1Ur5fvNpXk.Z1kl99-27F16mygVNcLG"
    tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
